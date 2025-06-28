9b31f8c92215f689a8c2d437cf85f3bc1bb2dee0
jerahmeel princewill <jamicojerahmeel@gmail.com>
2025-06-28 03:55:45 +0100

    feat: django admin site updates

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,44 @@
 from django.contrib import admin
+from .models import Author, Book, Genre, BookInstance
 
-# Register your models here.
+class BooksInline(admin.TabularInline):
+    model = Book
+    extra = 0
+
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+    fields = ['first_name', 'last_name', ('date_of_birth', 'date_of_death')]
+    inlines = [BooksInline]
+
+class BooksInstanceInline(admin.TabularInline):
+    model = BookInstance
+    extra = 0
+    
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+     list_display = ('title', 'author', 'display_genre')
+     inlines = [BooksInstanceInline]
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    list_display = ('book', 'status', 'due_back', 'id') 
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+
+# admin.site.register(Author)
+
+# admin.site.register(Book)
+admin.site.register(Genre)
+# admin.site.register(BookInstance)
+# admin.site.register(Language)
+       
--- locallibrary/catalog/models.py
+++ locallibrary/catalog/models.py
@@ -40,6 +40,7 @@ class Genre(models.Model):
         return reverse("genre-detail", args=[str(self.id)])
     
     
+    
      
     
 
@@ -62,6 +63,12 @@ class Book (models.Model):
     def get_absolute_url(self):
         # Returns the URL to access a detail record for this book
         return reverse('book-detail', args=[str(self.id)])
+    
+    def display_genre(self):
+    #Create a string for the Genre. This is required to display genre in Admin.
+        return ', '.join([genre.name for genre in self.genre.all()[:3]])
+
+    display_genre.short_description = 'Genre'
     
     
     
